094f5c320927af0a21816c43706b5e512f3973be
brunnofam <48735353+brunnofam@users.noreply.github.com>
2019-03-22 16:13:42 -0300

    First Part Completed
    Ainda falta fazer o salvamento dos dados. 
    XD!!

--- EstudoStruct.c
+++ EstudoStruct.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define VETMAX 50
 
+int cab();
 void terminar();
-float calcularMedia(float, float); 
+float calcularMedia2(float, float); 
 
-float calcularMedia(float n1, float n2){
-	float media = 0.0;
-	media  = (n1 + n2)/ 2.0;
-	return media;
-}
+	int raTemp; 
+	char cursoTemp;
+	char nomeTemp[30] = "INDEFINIDO"; 
+	char cursoExtTemp[20] = "INDEFINIDO";
+	float nota1Temp = 0.0, nota2Temp = 0.0, mediaTemp = 0.0 ; 
+	char situacaoTemp[15] = "INDEFINIDO";
+	
 
 int main (){
-
+	
 	struct teste {
 		
 		int ra; 
@@ -21,80 +25,162 @@ int main (){
 		float nota1, nota2;
 		float media; 
 		char situacao[15];
-		int opc;
+		char cursoExt[20]; 
+		
+		
 	};
-	
-	struct teste aluno[4];
-	
-	int ra; 
-	char curso;
-	char nome[30]; 
-	float nota1, nota2;
-	float media; 
-	char situacao[15];
-	int opc;
-	int i, j;
-	
-	
-
-	printf("Relacao de Alunos\n");
-	
-	for (i = 0; i< 1; i++){
 		
-		struct teste aluno[i];
-		
-		
+	struct teste vetAluno[VETMAX]; 
+	int opc = 1, inVet = 0, i = 0; ;
 	
-		printf("   RA   :    CURSO    :    NOME    :     NOTA 1     :     NOTA 2     :    MEDIA    :    SITUACAO    \n\n");
 	
+	while ((opc != 0 ) && (inVet < VETMAX)) {
+				
+		cab(); 
+		printf("\n\nRA: "); 
+		scanf("%d", &raTemp); 
 		
-		printf("RA: "); 
-		scanf("%d", &aluno[i].ra); fflush(stdin);
+		vetAluno[inVet].ra = raTemp;
+			
+		cab(); 
 		
-		printf("\nCURSO: MECANICA      [m]\n");
-		printf("CURSO: MECATRONICA   [t]\n");
-		printf("CURSO: ELETRONICA    [e]\n");
-		printf("CURSO: COMPUTACAO    [c]\n");
-		printf("CURSO: CIVIL         [v]\n");
-		printf("CURSO: AGRONOMICA    [a]\n\n");
+		printf("\nCURSO: MECANICA      [M]\n");
+		printf("CURSO: MECATRONICA   [T]\n");
+		printf("CURSO: ELETRONICA    [E]\n");
+		printf("CURSO: COMPUTACAO    [C]\n");
+		printf("CURSO: CIVIL         [V]\n");
+		printf("CURSO: AGRONOMICA    [A]\n");
+		printf("CURSO: ARQUITETURA   [Q]\n");
+		printf("CURSO: PRODUCAO      [P]\n");
+		printf("CURSO: ALIMENTOS     [F]\n\n");	
 		
+			
 		printf("OPCAO: "); 
-		scanf("%c", &aluno[i].curso); fflush(stdin); 
+		scanf("%c",  &cursoTemp); 
+		vetAluno[inVet].curso = cursoTemp;
 		
-		system("CLS"); 
+		switch (cursoTemp)  {
+			case 'F':
+				strcpy(cursoExtTemp, "ALIMENTOS");
+				break;
+			case 'P':
+				strcpy(cursoExtTemp, "PRODUCAO");
+				break;			
+			case 'Q':
+				strcpy(cursoExtTemp, "ARQUITETURA");											
+				break;
+			case 'V':
+				strcpy(cursoExtTemp, "CIVIL");
+				break;
+			case 'T':
+				strcpy(cursoExtTemp, "MECATRONICA");
+				break;
+			case 'C':
+				strcpy(cursoExtTemp, "COMPUTACAO");
+				break;
+			case 'A':
+				strcpy(cursoExtTemp, "AGRONOMICA");
+				break;
+			case 'E':
+				strcpy(cursoExtTemp, "ELETRICA");
+				break;
+			case 'M':
+				strcpy(cursoExtTemp, "MECANNICA");
+				break;
+			default:
+				strcpy(cursoExtTemp, "OUTRO");
+				break;
+		}
+		strcpy(vetAluno[inVet].cursoExt, cursoExtTemp);
+		cab();
+	
 		
-		
-		printf("NOME: ");
-		scanf("%30[^\n]s", &aluno[i].nome);
-		printf("%c",aluno[i].nome);
+		printf("\n\nNOME: ");
+		scanf("%30[^\n]s", nomeTemp);
+		strcpy(vetAluno[inVet].nome, nomeTemp);
+		cab(); 
 		
 		printf("\nINFORMAR A NOTA 1: "); 
-		scanf("%f", &aluno[i].nota1); fflush(stdin); 
+		scanf("%f", &nota1Temp); 
+		vetAluno[inVet].nota1 = nota1Temp;
+		cab();
+	
 		printf("\nINFORMAR A NOTA 2: "); 
-		scanf("%f", &aluno[i].nota2); fflush(stdin); 
+		scanf("%f", &nota2Temp); 
+		vetAluno[inVet].nota2 = nota2Temp;
 		
-		media  = calcularMedia(nota1, nota2); 
-		
-		if (media >= 5.0 ) {
-			strcpy(situacao, "APROVADO");
-		
+		mediaTemp  = calcularMedia2(nota1Temp, nota2Temp); 
+		vetAluno[inVet].media = mediaTemp;
+		if (nota1Temp, nota2Temp> 10 ){
+			printf("\n\n\nINSIRA UMA NOTA VALIDA!!!\n\n\n");
+			system("PAUSE");
+			terminar();
+			return 0;
+			
 		}
-		else{
-			strcpy(situacao, "REPROVADO");
+		if (mediaTemp >= 5.0) {
+			strcpy(situacaoTemp, "APROVADO");
 		}
-		
-		system("CLS"); 
+		else{	
+			strcpy(situacaoTemp, "REPROVADO");		
+		}
+		strcpy(vetAluno[inVet].situacao, situacaoTemp);
+		cab(); 	
+		inVet++;
+		system("PAUSE");
+		system("CLS");
+		printf("==================================================CADASTRO DOS ALUNOS==================================================\n\n\n");  
+		printf("DESEJA INSIRAR UMM NOVO ALUNO? \n\nSAIR      [0]\nCONTINUAR [1]\n\n\n\n");
+		scanf("%d", &opc); 
+	
+	if (opc == 0){
+		break;
 	}
 	
+	}
+
+	system("CLS"); 
+	printf("==================================================CADASTRO DOS ALUNOS==================================================\n\n\n"); 
+	printf("RELACAO DE ALUNOS\n\n");
+	printf("RA\tCURSO EXt\tNOME\tNOTA 1\tNOTA 2\tMEDIA\tSITUACAO\n\n");
 	
-	printf("                                                 RELACAO DE ALUNOS \n\n");
-	printf("   RA: %d  :    CURSO: %c    :    NOME:  %s   :     NOTA 1:  %.1f    :     NOTA 2:  %.1f     :    MEDIA:  %.1f   :    SITUACAO: %s  \n", aluno[i].ra, aluno[i].curso, aluno[i].nome, aluno[i].nota1, aluno[i].nota2, aluno[i].media, aluno[i].situacao);
 	
-	printf("\nDeseja informar uma nova nota? \n\nPara sim digite 1 para nao digite 2\n\n"); 
+  	for (i = 0 ; i < inVet; i++)  {
+  	printf("%d", vetAluno[i].ra);
+  	printf("\t%s", vetAluno[i].cursoExt);
+  	printf("\t%s", vetAluno[i].nome);
+  	printf("\t%.1f", vetAluno[i].nota1);
+  	printf("\t%.1f", vetAluno[i].nota2);
+  	printf("\t%.1f", vetAluno[i].media);
+  	printf("\t%s\n", vetAluno[i].situacao);
 	
-	
+	}
+	system("PAUSE"); 
+	terminar(); 
 	return 0;
-	}
+		
+}
 	
+int cab(){
+	
+	fflush(stdin); 
+	system("CLS");	
+	printf("==================================================CADASTRO DOS ALUNOS==================================================\n\n\n"); 
+	printf("RA: %d  :    CURSO: %s    :    NOME:   %s  :     NOTA 1  %.1f    :      NOTA 2:  %.1f    :      MEDIA:  %.1f    :      SITUACAO: %s  \n", raTemp, cursoExtTemp, nomeTemp, nota1Temp, nota2Temp, mediaTemp, situacaoTemp);
+	
+	return 0; 
+	
+}
+
+float calcularMedia2(float n1, float n2){
+	float media = 0.0;
+	media  = (n1 + n2)/ 2.0;
+	return media;
+}
+		
+void terminar(){
+	system("CLS");
+	printf("FIM DE PROGRAMA!!!\n\n\n\n\n\n\n\nPrograma feito por:\nBRUNO RACHID\n\n\n");
 
 
+}
